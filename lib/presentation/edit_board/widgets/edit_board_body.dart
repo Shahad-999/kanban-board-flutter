f4d16fb2f4b010f81531cc73b664b2fbc45f5202
Shahad-999 <shahad.h.999.a@gmail.com>
2023-03-26 03:06:04 +0300

    design all screen

--- lib/presentation/edit_board/widgets/edit_board_body.dart
+++ lib/presentation/edit_board/widgets/edit_board_body.dart
@@ -1,14 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:size_config/size_config.dart';
 
 import '../../create_board/widgets/input_view.dart';
-import 'edit_text_feild.dart';
+import 'edit_text_field.dart';
 
 class EditBoardBody extends StatelessWidget {
-   EditBoardBody({Key? key}) : super(key: key);
-  final TextEditingController _titleController = TextEditingController(text: 'Learning Flutter');
-  final TextEditingController _descriptionController = TextEditingController(text: 'Some Words :) ');
-//TODO LATER NEED REFACTOR
+   const EditBoardBody({Key? key}) : super(key: key);
+
+   _updateTitle(String title){
+     print('title $title');
+   }
+   _updateDescription(String description){
+     print('description $description');
+   }
+   _updateItemAtList(String text,int index){
+     print('text $text index $index');
+   }
+
+   //TODO LATER NEED REFACTOR
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -21,7 +30,8 @@ class EditBoardBody extends StatelessWidget {
             textField: EditTextField(
               maxLength: 30,
               maxLine: 1,
-              textController: _titleController ,
+              textEditingController: TextEditingController(text: 'Learning Flutter'),
+              onChange: _updateTitle,
             ),
         ),
         InputView(
@@ -31,10 +41,50 @@ class EditBoardBody extends StatelessWidget {
             textField: EditTextField(
               maxLength: 200,
               maxLine: 6,
-              textController: _descriptionController ,
+              textEditingController: TextEditingController(text: 'Some Words'),
+              onChange: _updateDescription,
             ),
         ),
-        SizedBox(height: 16.h)
+        SizedBox(height: 16.h),
+        Text(
+          'Lists',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w500
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: 4,
+            itemBuilder: (context,index){
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Dismissible(
+                  onDismissed: (_){
+                    //TODO LATER DELETE ITEM
+                  },
+                    key: ValueKey("Test $index"),
+                    child: EditTextField(maxLength: 10, maxLine: 1, onChange: (v){_updateItemAtList(v, index);},textEditingController: TextEditingController(text:'Test'),)
+                ),
+              );
+            }),
+        SizedBox(height: 8.h),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: TextButton(
+            onPressed: (){
+              //TODO SHOW DIALOG
+            },
+            child: Text(
+              'Delete this Board',
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500,
+                color: Colors.red
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
