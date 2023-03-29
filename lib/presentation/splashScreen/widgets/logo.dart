177eae6bd08adbda595d6532bb9fc2ac5cf55aec
Shahad-999 <shahad.h.999.a@gmail.com>
2023-03-29 07:29:07 +0300

    edit splash screen

--- lib/presentation/splashScreen/widgets/logo.dart
+++ lib/presentation/splashScreen/widgets/logo.dart
@@ -19,8 +19,10 @@ class _LogoState extends State<Logo> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    var controller1 = AnimationController(duration: const Duration(milliseconds: 1000), vsync: this);
-    var controller2 = AnimationController(duration: const Duration(milliseconds: 1500), vsync: this);
+    var controller1 = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+    var controller2 = AnimationController(
+        duration: const Duration(milliseconds: 1500), vsync: this);
 
     animation1 = Tween(begin: 0.0, end: 1.0).animate(controller1)
       ..addListener(() {
@@ -28,7 +30,6 @@ class _LogoState extends State<Logo> with TickerProviderStateMixin {
           _progress1 = animation1.value;
         });
       });
-
 
     animation2 = Tween(begin: 0.0, end: 1.0).animate(controller2)
       ..addListener(() {
@@ -38,19 +39,18 @@ class _LogoState extends State<Logo> with TickerProviderStateMixin {
       });
 
     // controller1.forward();
-    Timer(const Duration(milliseconds: 500),(){
+    Timer(const Duration(milliseconds: 500), () {
       controller1.forward();
     });
-    Timer(const Duration(milliseconds: 1500),(){
+    Timer(const Duration(milliseconds: 1500), () {
       controller2.forward();
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: LogoP(_progress1,_progress2),
+      painter: LogoP(_progress1, _progress2),
       child: const SizedBox(
         height: 178,
         width: 183,
@@ -59,38 +59,40 @@ class _LogoState extends State<Logo> with TickerProviderStateMixin {
   }
 }
 
-class LogoP extends CustomPainter{
+class LogoP extends CustomPainter {
   double progress1;
   double progress2;
-  late  Paint _paint;
-  LogoP(
-      this.progress1,
-      this.progress2
-      ){
-     _paint = Paint()
-      ..color = const Color(0xFF0075EB)
-      ..strokeWidth = 26
+  late Paint _paint;
+
+  LogoP(this.progress1, this.progress2) {
+    _paint = Paint()
+      ..color = const Color(0xFF024E9A)
+      ..strokeWidth = 36
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    var endX = 15 + (progress1 * (67 - 15));
-    var endY = 97 + (progress1 * (148 - 97));
-    canvas.drawLine(const Offset(15, 97), Offset(endX, endY), _paint);
+    const double startX1 = 5;
+    const double startY1 = 91;
+    const double startX2 = 72;
+    const double startY2 = 157;
+    var endX = startX1 + (progress1 * (startX2 - startX1));
+    var endY = startY1 + (progress1 * (startY2 - startY1));
+    canvas.drawLine(const Offset(startX1, startY1), Offset(endX, endY), _paint);
 
-    if(progress1 ==1){
-      var endX = 67 + (progress2 * (167 - 67));
-      var endY = 148 + (progress2 * (48 - 148));
-      canvas.drawLine(const Offset(67, 148), Offset(endX, endY), _paint);
+    if (progress1 == 1) {
+      _paint.color = const Color(0xFF0075EB);
+      var endX = startX2 + (progress2 * (183 - startX2));
+      var endY = startY2 + (progress2 * (47 - startY2));
+      canvas.drawLine(const Offset(startX2, startY2), Offset(endX, endY), _paint);
     }
-
   }
 
   @override
   bool shouldRepaint(covariant LogoP oldDelegate) {
-    return oldDelegate.progress1 != progress1 ||oldDelegate.progress2 != progress2 ;
+    return oldDelegate.progress1 != progress1 ||
+        oldDelegate.progress2 != progress2;
   }
-
 }
